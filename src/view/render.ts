e78b4aa086c9662121ce6a361b609b179608147a
hejny <me@pavolhejny.com>
2017-07-18 21:35:25 +0200

    snake sin

--- src/view/render.ts
+++ src/view/render.ts
@@ -40,21 +40,59 @@ export function render(ctx, game:IGame){
     ctx.lineCap="round";
     ctx.beginPath();
 
-    let lastSegment,i=0;
+
+    //todo better
+    const snakeLengthSum = (()=> {
+        let lastSegment, i = 0, snakeLengthSum = 0;
+        for (let thisSegment of game.snake.segments) {
+            i++;
+            if (lastSegment) {
+                snakeLengthSum += Math.sqrt(Math.pow(lastSegment.y - thisSegment.y, 2) + Math.pow(lastSegment.x - thisSegment.x, 2));
+            }
+            lastSegment = thisSegment;
+        }
+        return snakeLengthSum;
+    })();
+
+
+    let lastMoveBy=new Vector2(0,0);
+    let lastSegment,i=0,snakeLength=0;
     for(let thisSegment of game.snake.segments){
 
         i++;
 
         if(lastSegment){
+
+            snakeLength += Math.sqrt(Math.pow(lastSegment.y-thisSegment.y,2)+Math.pow(lastSegment.x-thisSegment.x,2));
+            const snakeFromHead = snakeLengthSum-snakeLength;
+
+            const amplitude = Math.min(Math.sqrt(snakeLength/10),5),
+                  periode = 10,
+                  shift = ((new Date()).getTime()-game.started)/100//todo pure
+            ;
+
+            const rotation = Math.PI/2 + Math.atan2(lastSegment.y-thisSegment.y,lastSegment.x-thisSegment.x);
+            const amplitude2 = Math.sin(snakeLength/periode+shift)*amplitude;
+
+            const thisMoveBy = new Vector2(
+                Math.cos(rotation)*amplitude2,
+                Math.sin(rotation)*amplitude2
+            );
+
             ctx.fillStyle = this.color;
-            ctx.lineWidth = Math.sqrt(game.snake.segments.length-i);//todo via real length
+            ctx.lineWidth = Math.sqrt(snakeFromHead);//todo via real length
             ctx.lineCap="round";
             ctx.beginPath();
 
 
-            ctx.moveTo(lastSegment.x-center.x,lastSegment.y-center.y);
-            ctx.lineTo(thisSegment.x-center.x,thisSegment.y-center.y);
+
+
+
+
+            ctx.moveTo(lastSegment.x-center.x+lastMoveBy.x,lastSegment.y-center.y+lastMoveBy.y);
+            ctx.lineTo(thisSegment.x-center.x+thisMoveBy.x,thisSegment.y-center.y+thisMoveBy.y);
             ctx.stroke();
+            lastMoveBy = thisMoveBy;
         }
         lastSegment = thisSegment;
 
