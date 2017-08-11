284585e6a15fd999cf44b2eb4d3edcd475dbe5a6
hejny <me@pavolhejny.com>
2017-08-11 17:47:37 +0200

    +mouse asset

--- src/render/render-foods.ts
+++ src/render/render-foods.ts
@@ -1,12 +1,44 @@
 import {IFood} from '../model/game'
 import {Vector2} from '../classes/vector2'
 
+
+
+var imgOrig = document.createElement("img");
+var img = document.createElement("canvas");
+//todo onload
+//todo process.env.PUBLIC_URL
+imgOrig.setAttribute('src', '/assets/images/objects/mouse/1.png');
+
+
+img.getContext('2d');
+
+//ctx.translate(image.width/2,image.height/2);
+//ctx.rotate(degrees*Math.PI/180);
+//ctx.drawImage(image,0,0,image.width,image.height,-image.width/2,-image.height   /2,image.width,image.height);
+//ctx.restore();
+
+
+
+
+
+
 //todo correct english word foods?
 export function renderFoods(ctx:CanvasRenderingContext2D, foods:IFood[], center:Vector2){
 
 
     for(let food of foods){
-        ctx.fillStyle = 'black';
+
+
+        ctx.drawImage(imgOrig,
+            food.position.x-center.x,
+            food.position.y-center.y,
+            50,
+            100
+        );
+
+
+
+        /*ctx.fillStyle = 'black';
         ctx.beginPath();
         ctx.arc(
             food.position.x-center.x,//+Math.cos(duration/100)*10,
@@ -14,7 +46,7 @@ export function renderFoods(ctx:CanvasRenderingContext2D, foods:IFood[], center:
 
             food.size / 2, 0, Math.PI * 2, true);
         ctx.closePath();
-        ctx.fill();
+        ctx.fill();*/
 
 
 
